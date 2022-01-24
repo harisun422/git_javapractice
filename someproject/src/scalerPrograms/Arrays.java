a063bdc0898c57affb2ae669135f17f97f59b84a
delluser <delluser@LAPTOP-6N2GONVC>
2022-01-25 00:21:04 +0530

    save

--- someproject/src/scalerPrograms/Arrays.java
+++ someproject/src/scalerPrograms/Arrays.java
@@ -9,6 +9,8 @@ public class Arrays {
 		
 		
 		//RotateArray(common,6);
+		//maxsubarraysum(common,6);
+		allsubarraysum(common);
 	}
 
 	//Rotate the Array by K positions
@@ -24,7 +26,32 @@ public class Arrays {
 	}
 	
 	public static void maxsubarraysum(int a[], int k) {
-		
+		int sum=0;
+		int [] b = new int[a.length];
+		for(int i=0;i<a.length;i++) {
+			sum =sum+a[i];
+			b[i]=sum;
+		System.out.println("b["+i+"] : "+b[i]);
+		}
+		int max=a[k-1];
+		for(int i=k;i<b.length;i++) {
+			System.out.println("maximum sum was : "+(b[i]-b[i-k]));
+			if(max<b[i]-b[i-k])
+				max=b[i]-b[i-k];
+			
+		}
+		System.out.println("maximum sum is : "+max);
 	}
-	
+
+	public static void allsubarraysum(int a[]) {
+		int sum=0;
+		int count =0;
+		for(int i=0;i<a.length;i++) {
+			for(int j=i;j<a.length;j++) {
+				sum=sum+a[i];
+				count++;
+				System.out.println(count +") : "+sum);
+			}
+		}
+	}
 }
